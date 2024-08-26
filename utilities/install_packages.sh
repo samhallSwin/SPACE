a0d4974f084902d6728c893c391a7cf0504d892f
myetea <166641366+myetea@users.noreply.github.com>
2024-08-27 02:26:38 +1000

    Delete utilities directory

--- utilities/install_packages.sh
+++
@@ -1,5 +0,0 @@
-# Iterate through each line in requirements.txt
-while IFS= read -r package || [[ -n "$package" ]]; do
-    # Install the package
-    pip install "$package"
-done < ../requirements.txt
