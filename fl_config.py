5c0b4a151528728d168c49daa343a265bea5c199
Connor-bett-swin <90178393+Connor-bett-swin@users.noreply.github.com>
2024-09-01 19:09:22 +1000

    Updated branch to main
    updated to reflect current naming standard
    
    updated fl_core config methods
    
    Renamed fl core file
    
    federated_learning.py

--- fl_config.py
+++ fl_config.py
@@ -14,6 +14,8 @@ Usage:
 """
 import json
 from dataclasses import dataclass
+from fl_core import FederatedLearning
+from model import *
 
 from interfaces.config import Config
 
@@ -27,17 +29,23 @@ class FLConfig(Config):
 
     # Constructor, accepts core module
     def __init__(self, federated_learning):
-        self.federated_learning = federated_learning
+        self.fl_core = FederatedLearning()
+        self.model = Model()
         self.options = None
+        self.model_options = None
 
     # Traverse JSON options to check for nested objects?
     def read_options(self, options):
         self.options = options
         self.set_federated_learning()
+        self.set_federated_learning_model()
 
     def read_options_from_file(file):
         return super().read_options_from_file()
 
-    def set_federated_learning(self):
-        pass
-        
+    def set_federated_learning(self) -> None:
+        self.fl_core.set_num_rounds(self.options["Num_rounds"])
+        self.fl_core.set_num_clients(self.options["Num_rounds"])
+
+    def set_federated_learning_model(self) -> None:
+        self.model.
