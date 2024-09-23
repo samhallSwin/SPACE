1cdd6923bc2c0ffeb9b77ec405ef7873a49f3262
elysiacodes <43836238+elysiacodes@users.noreply.github.com>
2024-09-24 00:26:23 +1000

    Moved flomps module args methods to be independent

--- cli_args.py
+++ cli_args.py
@@ -4,6 +4,28 @@ def setup_settings_parser(parser):
     parser.add_argument('--options-file', type=str, help="Set JSON Options File")
     parser.add_argument('--show-options', action='store_true', help="Display JSON options for module configuration")
 
+def add_sat_sim_args(parser):
+        print("Adding SAT_SIM args")
+        sat_sim_group = parser.add_argument_group(ModuleKey.SAT_SIM)
+
+        # Add args here
+        sat_sim_group.add_argument('--start-time', type=str, help='The start date/time for the satellite simulation')
+        sat_sim_group.add_argument('--end-time', type=str, help='The end date/time for the satellite simulation')
+        sat_sim_group.add_argument('--timestep', type=int, help='Timestep for the simulation')
+
+def add_algorithm_args(parser):
+    print("Adding ALGORITHM args")
+    algorithm_group = parser.add_argument_group(ModuleKey.ALGORITHM)
+
+    # Add args here
+
+def add_fl_args(parser):
+    print("Adding FL args")
+    fl_group = parser.add_argument_group(ModuleKey.FL)
+
+    # Add args here
+    fl_group.add_argument('--num-rounds', type=int, help='Number of Federated Learning rounds for the simulation')
+    fl_group.add_argument('--num-clients', type=int, help='Number of Federated Learning clients for the simulation')
 
 def setup_flomps_parser(parser):
     def add_positional_args(parser):
@@ -18,29 +40,6 @@ def setup_flomps_parser(parser):
         parser.add_argument('--algorithm-only', action='store_true', help="Run the Algorithm standalone. Requires an Adjacency Matrices (.am) file.")
         parser.add_argument('--fl-only', action='store_true', help="Run the Federated Learning standalone. Requires a Federated Learning Adjacency Matrices (.flam) file.")
         parser.add_argument('--model-only', action='store_true', help="Run the ML model standalone.")
-
-    def add_sat_sim_args(parser):
-        print("Adding SAT_SIM args")
-        sat_sim_group = parser.add_argument_group(ModuleKey.SAT_SIM)
-
-        # Add args here
-        sat_sim_group.add_argument('--start-time', type=str, help='The start date/time for the satellite simulation')
-        sat_sim_group.add_argument('--end-time', type=str, help='The end date/time for the satellite simulation')
-        sat_sim_group.add_argument('--timestep', type=int, help='Timestep for the simulation')
-
-    def add_algorithm_args(parser):
-        print("Adding ALGORITHM args")
-        algorithm_group = parser.add_argument_group(ModuleKey.ALGORITHM)
-
-        # Add args here
-
-    def add_fl_args(parser):
-        print("Adding FL args")
-        fl_group = parser.add_argument_group(ModuleKey.FL)
-
-        # Add args here
-        fl_group.add_argument('--num-rounds', type=int, help='Number of Federated Learning rounds for the simulation')
-        fl_group.add_argument('--num-clients', type=int, help='Number of Federated Learning clients for the simulation')
 
     add_positional_args(parser)
     add_options_args(parser)
