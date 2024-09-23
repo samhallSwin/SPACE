02e8774f9304a8931d4a5a148f2796842e1c6237
myetea <166641366+myetea@users.noreply.github.com>
2024-09-23 20:29:12 +1000

    Create main_test.py

---
+++ main_test.py
@@ -0,0 +1,19 @@
+"""
+Filename: main_test.py
+Description: Test file for model.py
+Author: Ahmed Mubeenul Azim & Michelle Abrigo
+Date: 2024-09-23
+Version: 1.0
+Python Version: 
+
+Changelog:
+- 2024-08-02: Initial creation, setup & test case creation.
+
+Usage: Do 'pytest main_test.py' to run the test session
+
+"""
+from main import 
+import tensorflow as tf
+import numpy as np
+from unittest.mock import patch
+import pytest
