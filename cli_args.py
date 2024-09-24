97508087b695d41567f987e68a7d6ac9aa1d6256
Md Nahid Tanjum <165870804+nahid-tanjum@users.noreply.github.com>
2024-09-25 06:56:37 +1000

    add gui argument inside sat_sim_args

--- cli_args.py
+++ cli_args.py
@@ -27,6 +27,9 @@ def setup_flomps_parser(parser):
         sat_sim_group.add_argument('--start-time', type=str, help='The start date/time for the satellite simulation')
         sat_sim_group.add_argument('--end-time', type=str, help='The end date/time for the satellite simulation')
         sat_sim_group.add_argument('--timestep', type=int, help='Timestep for the simulation')
+        #gui args
+        sat_sim_group.add_argument('--gui', action='store_true', help='Enable GUI mode for satellite simulation')
+
 
     def add_algorithm_args(parser):
         print("Adding ALGORITHM args")
