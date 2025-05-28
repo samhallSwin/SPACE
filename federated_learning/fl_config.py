d76a25ff0cd315dde92766ccc73abbcc21a00f7f
NicholasPaulC <nicholaspaulcandra@gmail.com>
2025-05-28 19:05:22 +1000

    Changing old file into the updated code

--- federated_learning/fl_config.py
+++ federated_learning/fl_config.py
@@ -1,24 +1,14 @@
-"""
-Filename: fl_config.py
-Description: Converts Federated Learning JSON options to local data types and configures core Federated Learning module via setters. 
-Author: Elysia Guglielmo and Connor Bett
-Date: 2024-08-02
-Version: 1.0
-Python Version: 
-
-Changelog:
-- 2024-08-02: Initial creation.
-- 2024-09-16: Supported standalone execution, refactored Model Setters
-
-Usage: 
-
-"""
 import json
 from dataclasses import dataclass
+import sys
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from federated_learning.fl_core import FederatedLearning
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from model import *
-
-from interfaces.config import Config
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from interfaces.output import Output
 
 @dataclass
 class AlgorithmOptions:
@@ -26,27 +16,33 @@ class AlgorithmOptions:
     node_processing_time: int
     search_depth: int
 
-class FLConfig(Config):
+class Config:
 
-    # Constructor, accepts core module
     def __init__(self, federated_learning: FederatedLearning):
         self.fl_core = federated_learning
         self.model = Model()
         self.options = None
         self.model_options = None
 
-    # Traverse JSON options to check for nested objects?
-    def read_options(self, options):
+    def read_options(self, options: dict):
         self.options = options
         self.set_federated_learning_model()
         self.set_federated_learning()
 
-    def read_options_from_file(file):
-        return super().read_options_from_file()
+    def read_options_from_file(self, file_path: str):
+        with open(file_path, 'r') as file:
+            full_config = json.load(file)
+        options = full_config.get("federated_learning", {})
+        self.read_options(options)
 
     def set_federated_learning(self) -> None:
         self.fl_core.set_num_rounds(self.options["num_rounds"])
         self.fl_core.set_num_clients(self.options["num_clients"])
+
+        self.fl_core.print_config_summary(
+        model_type=self.options["model_type"],
+        data_set=self.options["data_set"]
+        )
         print ("FL setters called")
 
     def set_federated_learning_model(self) -> None:
@@ -55,3 +51,10 @@ class FLConfig(Config):
         self.fl_core.set_model(self.model)
         print ("MODEL setters called")
 
+if __name__ == "__main__":
+    from federated_learning.fl_core import FederatedLearning
+
+    fl_instance = FederatedLearning()
+    config = Config(fl_instance)
+
+    config.read_options_from_file("options.json")
