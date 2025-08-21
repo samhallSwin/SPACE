3bcdbc919e8042551251701df73b990ffe698f6a
Kartik Punna <kartikpunna@gmail.com>
2025-08-21 18:04:48 +1000

    Adding some tests to help Emelee get started

--- GUI/Tests/gui_tests.py
+++ GUI/Tests/gui_tests.py
@@ -16,7 +16,7 @@ class TestGUIWindow(unittest.TestCase):
         script_path = Path("GUI") / "Application.py"
         cls.proc = subprocess.Popen([str(python_exe), str(script_path)])
 
-        time.sleep(2)
+        time.sleep(1)
 
         cls.app = Application(backend="uia").connect(title="S.P.A.C.E")
         cls.window = cls.app.window(title="S.P.A.C.E")
@@ -32,9 +32,17 @@ class TestGUIWindow(unittest.TestCase):
         button = self.window.child_window(title="Midnight", control_type="Button")
         self.assertTrue(button.exists())
         # button.click()
+        
+    # Tests for Emelee:
+    # Need to update later as timer's name is the actual time.
+    def test_click_midnight_button(self):
+        pass
+    
+    def test_slider_time_exists(self):
+        pass
 
-    def test_print_all_elements(self):
-        self.window.print_control_identifiers()
+    # def test_print_all_elements(self):
+    #     self.window.print_control_identifiers()
 
 if __name__ == "__main__":
     unittest.main() 
